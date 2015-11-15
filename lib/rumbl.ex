4a86eb746dadea6e4d9d3e3e212191603cc2fbf6
John Hogarty <hogihung@gmail.com>
2015-11-14 20:07:53 -0500

    Complete Chapter Four.

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -10,7 +10,7 @@ defmodule Rumbl do
       # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
       # Start the Ecto repository
-      # worker(Rumbl.Repo, []),   # commented out by jfhogarty on 11/14/15 @ 03:56pm est
+      worker(Rumbl.Repo, []),
       # Here you could define other workers and supervisors as children
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
     ]
